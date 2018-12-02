7f04c20b333a65fad231cc7a3cdcd1f6e473cd79
Zhenyu Yang <zhenyuyang@ucsb.edu>
2018-12-02 10:53:06 -0800

    fixed bug

--- readNames.m
+++ readNames.m
@@ -1,6 +1,6 @@
 function peopleSet = readNames()
 
-fid1 = fopen('data/10/hetongzhi_all_people.csv','r', 'n','UTF-8'); %# open csv file for reading
+fid1 = fopen('data/11/hetongzhi_all_people.csv','r', 'n','UTF-8'); %# open csv file for reading
 peopleSet = {};
 peopleCount = 1;
 
@@ -17,18 +17,18 @@ fclose(fid1);
 
 
 
-% 
-% 
-% fid1 = fopen('disanfang_all_people.csv','r', 'n','UTF-8'); %# open csv file for reading
-% 
-% while ~feof(fid1)
-%     line = fgets(fid1); %# read line by line
-%     lineSplit = strsplit(line,',','CollapseDelimiters',false);
-%     
-%     tempPeople = peopleBuilder(lineSplit{6},lineSplit{2},lineSplit{10});
-%     a = 1;
-%     peopleSet{peopleCount} = tempPeople;
-%     peopleCount = peopleCount + 1;
-% end
-% fclose(fid1);
+
+
+fid1 = fopen('data/11/disanfang_all_people.csv','r', 'n','UTF-8'); %# open csv file for reading
+
+while ~feof(fid1)
+    line = fgets(fid1); %# read line by line
+    lineSplit = strsplit(line,',','CollapseDelimiters',false);
+    
+    tempPeople = peopleBuilder(lineSplit{6},lineSplit{2},lineSplit{10});
+    a = 1;
+    peopleSet{peopleCount} = tempPeople;
+    peopleCount = peopleCount + 1;
+end
+fclose(fid1);
 % end
