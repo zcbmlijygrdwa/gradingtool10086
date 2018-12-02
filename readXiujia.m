3fd54c57da69198116067567c7dd15f0d93f2eab
Zhenyu Yang <zhenyuyang@ucsb.edu>
2018-12-02 00:01:36 -0800

    fixed bug: adding previous xiujia when found canclled

--- readXiujia.m
+++ readXiujia.m
@@ -3,7 +3,7 @@ function xiujiaSet = readXiujia()
 xiujiaSet = {};
 xiujiaSet_Count = 1;
 
-fid1 = fopen('data/xiujia201810261711188971.csv','r', 'n','UTF-8'); %# open csv file for reading
+fid1 = fopen('data/10/10xiujia.csv','r', 'n','UTF-8'); %# open csv file for reading
 
 while ~feof(fid1)
     line = fgets(fid1); %# read line by line
@@ -11,9 +11,10 @@ while ~feof(fid1)
     status = lineSplit{12}(1:end-2);
     if(strcmp(status,'complete')||strcmp(status,'pending'))
     xiujia = xiujiaBuilder(lineSplit{2}(2:end),lineSplit{8}(2:end),lineSplit{9}(2:end));
-    end
     xiujiaSet{xiujiaSet_Count} = xiujia;
     xiujiaSet_Count = xiujiaSet_Count + 1;
+    end
+    
 end
 fclose(fid1);
 end
